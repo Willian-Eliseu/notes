fee828ee94142731c5d3d0706d72597dbbe0ce6c
willian Eliseu da Silva <willian.eliseu@hotmail.com>
2025-08-06 06:30:10 -0300

    added query for get user data on auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -35,19 +36,58 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        //test database connection
-        try{
-            DB::connection()->getPdo();
-            echo 'Connection is OK!';
-        }catch(\PDOException $e){
-            echo "Connection failed: " . $e->getMessage();
+        /*
+        //get all the users from the database
+        $users = User::all()->toArray();
+        echo '<pre>';
+        print_r($users);
+
+        //as an object instance of the model's class
+        $userModel = new User();
+        $users = $userModel->all()->toArray();
+        echo '<pre>';
+        print_r($users);
+        */
+
+        //check if user exists
+        $user = User::where('username', $username)
+                    ->where('deleted_at', NULL)
+                    ->first();
+
+        if(!$user){
+            return  redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Username ou password incorretos.');
         }
 
-        echo 'Fim!';
+        //check if password is correct
+        if(!password_verify($password, $user->password)){
+            return  redirect()
+                    ->back()
+                    ->withInput()
+                    ->with('loginError', 'Username ou password incorretos.');
+        }
+
+        //update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        //login user
+        session([
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username
+            ]
+        ]);
+
+        echo 'LOGIN COM SUCESSO';
     }
 
     public function logout()
     {
-        return view('logout');
+        //logout from the application
+        session()->forget('user');
+        return redirect()->to('/login');
     }
 }
